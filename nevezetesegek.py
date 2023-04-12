f6ff36c89e8c765e8b2d052db88ffcdba45ac7ba
perakmarton <92153091+perakmarton@users.noreply.github.com>
2023-04-12 08:02:50 +0200

    Add files via upload

---
+++ nevezetesegek.py
@@ -0,0 +1,5 @@
+nevezetessegek = ["Hollókő","Őrség","A villányi borvidék"," Esztergom"," Sopron","Tihany és a Tihanyi-félsziget","Pécs"]
+for i in range(0, len(nevezetessegek)):
+    print(i+1," ",nevezetessegek[i])
+valasztas= int (input("Melyiket válsztod? Add meg a számjelét!"))
+valasztott_index=valasztas-1
